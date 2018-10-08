aa10ab518aa87631462fc2fc80e211141aafabaf
zhuyu <757693435@qq.com>
2018-10-08 14:07:39 +0800

    添加tablayout和viewpager

--- app/src/main/java/com/zhuyu/idouban/HomeActivity.java
+++ app/src/main/java/com/zhuyu/idouban/HomeActivity.java
@@ -1,7 +1,23 @@
 package com.zhuyu.idouban;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.zhuyu.idouban.book.BooksFragment;
+import com.zhuyu.idouban.movie.MoviesFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +25,67 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        ViewPager viewPager=findViewById(R.id.home_viewPager);
+        TabLayout tabLayout=findViewById(R.id.home_tablayout);
+        setViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+
+    private void setViewPager(ViewPager viewpager){
+        DouBanPagerAdapter adapter=new DouBanPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new BooksFragment(),"书籍");
+        adapter.addFragment(new MoviesFragment(),"电影");
+        viewpager.setAdapter(adapter);
+
+    }
+
+
+
+    private class DouBanPagerAdapter extends FragmentPagerAdapter{
+
+        private List<Fragment> mFragmentList=new ArrayList<>();
+        private List<String> mTitles=new ArrayList<>();
+
+        public DouBanPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+
+        public void addFragment(Fragment fragment ,String title){
+            mFragmentList.add(fragment);
+            mTitles.add(title);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles.get(position);
+        }
+    }
+
 }
